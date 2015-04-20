9d3e3e9be3d8a6ac1c0f610560b2ed2493097c69
B64283 <mattdarke@fullsail.edu>
2015-04-19 20:05:23 -0700

    milestone one still needs save correct data

--- app/src/main/java/com/example/matthewdarke/myrecipebox/MainActivity.java
+++ app/src/main/java/com/example/matthewdarke/myrecipebox/MainActivity.java
@@ -4,23 +4,61 @@ import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, Serializable, AppetizerFragment.OnFragmentMediaControllInteractionListener, SaladFragment.OnFragmentMediaControllInteractionListener, MexicanFragment.OnFragmentMediaControllInteractionListener, BeefFragment.OnFragmentMediaControllInteractionListener, ChickenFragment.OnFragmentMediaControllInteractionListener, ShoppingFragment.OnFragmentMediaControllInteractionListener       {
 
+
+
+    Button bnAdd;
+    private static final long serialVersionUID = 2736847634070552888L;
+
+    //set up arrayList and array adapter with model data
+    public static ArrayList<Items> itemsArray = new ArrayList<>();
+
+    public static ArrayAdapter<Items> adapter;
+
+
+    //requestCode - the identifier from the launching intent
+    public static final int REQUEST_CODE = 1;
+
+    //model data member var
+    public static Items mItmData;
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
 
+
+
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    public AppetizerFragment fragment1;
+    public SaladFragment fragment2;
+    public ChickenFragment fragment3;
+    public BeefFragment fragment4;
+    public MexicanFragment fragment5;
+    public ShoppingFragment fragment6;
+
+    public android.app.FragmentManager fragmentManager;
+    public FragmentTransaction fragTrans;
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
@@ -38,8 +76,21 @@ public class MainActivity extends Activity
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
+
                 (DrawerLayout) findViewById(R.id.drawer_layout));
+
+        fragment1 = (AppetizerFragment)android.app.Fragment.instantiate(this, AppetizerFragment.class.getName(), null);
+        fragment2 = (SaladFragment) android.app.Fragment.instantiate(this, SaladFragment.class.getName(), null);
+        fragment3 = (ChickenFragment) android.app.Fragment.instantiate(this, ChickenFragment.class.getName(), null);
+        fragment4 = (BeefFragment) android.app.Fragment.instantiate(this, BeefFragment.class.getName(), null);
+        fragment5 = (MexicanFragment) android.app.Fragment.instantiate(this, MexicanFragment.class.getName(), null);
+        fragment6 = (ShoppingFragment) android.app.Fragment.instantiate(this, ShoppingFragment.class.getName(), null);
+
+
+
     }
+
+
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
@@ -53,14 +104,531 @@ public class MainActivity extends Activity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
+                fragmentManager = getFragmentManager();
+                fragTrans = fragmentManager.beginTransaction();
+                fragTrans.replace(R.id.container, fragment1);
+                fragTrans.commit();
+
                 mTitle = getString(R.string.title_section1);
+
+
+
                 break;
             case 2:
+                fragmentManager = getFragmentManager();
+                fragTrans = fragmentManager.beginTransaction();
+                fragTrans.replace(R.id.container, fragment2);
+                fragTrans.commit();
+
+
                 mTitle = getString(R.string.title_section2);
                 break;
             case 3:
+
+                fragmentManager = getFragmentManager();
+                fragTrans = fragmentManager.beginTransaction();
+                fragTrans.replace(R.id.container, fragment3);
+                fragTrans.commit();
+
                 mTitle = getString(R.string.title_section3);
                 break;
+            case 4:
+                fragmentManager = getFragmentManager();
+                fragTrans = fragmentManager.beginTransaction();
+                fragTrans.replace(R.id.container, fragment4);
+                fragTrans.commit();
+
+
+
+                mTitle = getString(R.string.title_section4);
+                break;
+            case 5:
+
+                fragmentManager = getFragmentManager();
+                fragTrans = fragmentManager.beginTransaction();
+                fragTrans.replace(R.id.container, fragment5);
+                fragTrans.commit();
+
+
+                mTitle = getString(R.string.title_section5);
+                break;
+            case 6:
+                fragmentManager = getFragmentManager();
+                fragTrans = fragmentManager.beginTransaction();
+                fragTrans.replace(R.id.container, fragment6);
+                fragTrans.commit();
+
+                mTitle = getString(R.string.title_section6);
+                break;
+
+        }
+    }
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        AppetizerFragment mainFragment = (AppetizerFragment) getFragmentManager()
+                .findFragmentById(R.id.container);
+
+        BeefFragment beefFragment = (BeefFragment) getFragmentManager()
+                .findFragmentById(R.id.container);
+
+        ChickenFragment chickenFragment = (ChickenFragment) getFragmentManager()
+                .findFragmentById(R.id.container);
+
+        MexicanFragment mexicanFragment = (MexicanFragment) getFragmentManager()
+                .findFragmentById(R.id.container);
+
+        SaladFragment saladFragment = (SaladFragment) getFragmentManager()
+                .findFragmentById(R.id.container);
+
+        ShoppingFragment shoppingFragment = (ShoppingFragment) getFragmentManager()
+                .findFragmentById(R.id.container);
+
+        if (mainFragment != null) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Indicate state of results
+            if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+
+                ArrayList<Items> first = (ArrayList<Items>) data.getSerializableExtra("conDat");
+
+                if (itemsArray != null) {
+
+                    itemsArray.addAll(first);
+
+                    adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+                    mainFragment.setListAdapter(adapter);
+
+
+                    saveData();
+                }
+
+            }
+
+        }  else if (beefFragment != null) {
+
+                super.onActivityResult(requestCode, resultCode, data);
+
+                // Indicate state of results
+                if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+
+                    ArrayList<Items> first = (ArrayList<Items>) data.getSerializableExtra("conDat");
+
+                    if (itemsArray != null) {
+
+                        itemsArray.addAll(first);
+
+                        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+
+                        beefFragment.setListAdapter(adapter);
+                        saveData();
+                    }
+
+                }
+
+            }  else if  (chickenFragment != null) {
+
+                    super.onActivityResult(requestCode, resultCode, data);
+
+                    // Indicate state of results
+                    if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+
+                        ArrayList<Items> first = (ArrayList<Items>) data.getSerializableExtra("conDat");
+
+                        if (itemsArray != null) {
+
+                            itemsArray.addAll(first);
+
+                            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+
+
+                            chickenFragment.setListAdapter(adapter);
+                            saveData();
+                        }
+
+                    }
+
+        }  else if  (mexicanFragment != null) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Indicate state of results
+            if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+
+                ArrayList<Items> first = (ArrayList<Items>) data.getSerializableExtra("conDat");
+
+                if (itemsArray != null) {
+
+                    itemsArray.addAll(first);
+
+                    adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+
+
+                    mexicanFragment.setListAdapter(adapter);
+                    saveData();
+                }
+            }
+
+        }  else if  (saladFragment != null) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Indicate state of results
+            if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+
+                ArrayList<Items> first = (ArrayList<Items>) data.getSerializableExtra("conDat");
+
+                if (itemsArray != null) {
+
+                    itemsArray.addAll(first);
+
+                    adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+
+
+                    saladFragment.setListAdapter(adapter);
+                    saveData();
+                }
+
+            }
+
+        }  else if  (shoppingFragment != null) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+
+            // Indicate state of results
+            if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+
+
+                ArrayList<Items> first = (ArrayList<Items>) data.getSerializableExtra("conDat");
+
+                if (itemsArray != null) {
+
+                    itemsArray.addAll(first);
+
+                    adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+
+
+                    shoppingFragment.setListAdapter(adapter);
+                    saveData();
+                }
+
+
+
+            }
+
+        }
+    }
+
+    //saves data using File output stream stores to a file
+    public void saveData() {
+
+
+        try {
+            FileOutputStream outputStream = openFileOutput("contactDat", Context.MODE_PRIVATE);
+
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+            //add data items to array
+            for (Items iItemsArray : itemsArray) {
+
+                mItmData = iItemsArray;
+
+                //write objects
+                objectOutputStream.writeObject(mItmData);
+            }
+
+            objectOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //reads saved data
+    public void loadSavedData() {
+
+        AppetizerFragment mainFragment = (AppetizerFragment)
+                getFragmentManager().findFragmentById(R.id.container);
+
+
+
+        try {
+
+            FileInputStream inputStream = openFileInput("contactDat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            //needs while loop
+
+            while (inputStream.available() != 0) {
+
+                //cast data
+                mItmData = (Items) objectInputStream.readObject();
+
+                itemsArray.add(mItmData);
+                //close input stream
+            }
+
+            //close input stream
+
+            objectInputStream.close();
+//add objects to array adapter
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+            mainFragment.setListAdapter(adapter);
+
+
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //reads saved data
+    public void loadSavedSaladData() {
+
+        SaladFragment saladFragment = (SaladFragment)
+                getFragmentManager().findFragmentById(R.id.container);
+
+
+
+        try {
+
+            FileInputStream inputStream = openFileInput("contactDat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            //needs while loop
+
+            while (inputStream.available() != 0) {
+
+                //cast data
+                mItmData = (Items) objectInputStream.readObject();
+
+                itemsArray.add(mItmData);
+                //close input stream
+            }
+
+            //close input stream
+
+            objectInputStream.close();
+//add objects to array adapter
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+            saladFragment.setListAdapter(adapter);
+
+
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //reads saved data
+    public void loadSavedBeefData() {
+
+        BeefFragment saladFragment = (BeefFragment)
+                getFragmentManager().findFragmentById(R.id.container);
+
+
+
+        try {
+
+            FileInputStream inputStream = openFileInput("contactDat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            //needs while loop
+
+            while (inputStream.available() != 0) {
+
+                //cast data
+                mItmData = (Items) objectInputStream.readObject();
+
+                itemsArray.add(mItmData);
+                //close input stream
+            }
+
+            //close input stream
+
+            objectInputStream.close();
+//add objects to array adapter
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+            saladFragment.setListAdapter(adapter);
+
+
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //reads saved data
+    public void loadSavedChickenData() {
+
+        ChickenFragment saladFragment = (ChickenFragment)
+                getFragmentManager().findFragmentById(R.id.container);
+
+
+
+        try {
+
+            FileInputStream inputStream = openFileInput("contactDat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            //needs while loop
+
+            while (inputStream.available() != 0) {
+
+                //cast data
+                mItmData = (Items) objectInputStream.readObject();
+
+                itemsArray.add(mItmData);
+                //close input stream
+            }
+
+            //close input stream
+
+            objectInputStream.close();
+//add objects to array adapter
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+            saladFragment.setListAdapter(adapter);
+
+
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //reads saved data
+    public void loadSavedMexicanData() {
+
+        MexicanFragment mexFragment = (MexicanFragment)
+                getFragmentManager().findFragmentById(R.id.container);
+
+
+
+        try {
+
+            FileInputStream inputStream = openFileInput("contactDat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            //needs while loop
+
+            while (inputStream.available() != 0) {
+
+                //cast data
+                mItmData = (Items) objectInputStream.readObject();
+
+                itemsArray.add(mItmData);
+                //close input stream
+            }
+
+            //close input stream
+
+            objectInputStream.close();
+//add objects to array adapter
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+            mexFragment.setListAdapter(adapter);
+
+
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+    //reads saved data
+    public void loadSavedShoppingData() {
+
+        ShoppingFragment shopFragment = (ShoppingFragment)
+                getFragmentManager().findFragmentById(R.id.container);
+
+
+
+        try {
+
+            FileInputStream inputStream = openFileInput("contactDat");
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+            //needs while loop
+
+            while (inputStream.available() != 0) {
+
+                //cast data
+                mItmData = (Items) objectInputStream.readObject();
+
+                itemsArray.add(mItmData);
+                //close input stream
+            }
+
+            //close input stream
+
+            objectInputStream.close();
+//add objects to array adapter
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, itemsArray);
+
+            shopFragment.setListAdapter(adapter);
+
+
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    @Override
+    public void onFragmentControllInteraction(View v) {
+
+        if (v.getId() == R.id.bnAdd) {
+
+            Intent addTo = new Intent(this, AddActivity.class);
+            startActivityForResult(addTo, REQUEST_CODE);
+
+
         }
     }
 
@@ -78,6 +646,8 @@ public class MainActivity extends Activity
             // Only show items in the action bar relevant to this screen
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
+
+
             getMenuInflater().inflate(R.menu.main, menu);
             restoreActionBar();
             return true;
@@ -85,24 +655,13 @@ public class MainActivity extends Activity
         return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
+
+        /**
+         * A placeholder fragment containing a simple view.
+         */
     public static class PlaceholderFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
